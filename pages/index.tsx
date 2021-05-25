0253cc6e05d546721957eb97f7b602d53b32a028
Hamilton Dias <hamiltondias1997@hotmail.com>
2021-05-24 21:19:40 -0300

    server side auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
+import { parseCookies  } from 'nookies'
+import { withSSRGuest } from '../utils/withSSRGuest';
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -28,3 +31,9 @@ export default function Home() {
     </form>
   )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})

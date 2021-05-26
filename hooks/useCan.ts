9f3e82a9b28d9359ff442fef2dbb7ea0695183a7
Hamilton Dias <hamiltondias1997@hotmail.com>
2021-05-25 21:09:38 -0300

    permissions

---
+++ hooks/useCan.ts
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { validadeUserPermissions } from "../utils/validadeUserPermissions"
+
+type UserCanParams = {
+    permissions?: string[];
+    roles?: string[];
+}
+
+export function useCan({ permissions, roles }: UserCanParams) {
+    const { user, isAuthenticated } = useContext(AuthContext)
+
+    if (!isAuthenticated) {
+        return false
+    }
+
+    const userHasValidPermissions = validadeUserPermissions({user, permissions, roles})
+
+    return userHasValidPermissions
+}
